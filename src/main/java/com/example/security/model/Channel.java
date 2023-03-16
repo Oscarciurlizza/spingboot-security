f3c909c2af6292e536f84a6ae0452bf070e34108
Oscar <ciurlizza1000@gmail.com>
2023-03-16 16:10:01 -0500

    Api Login

---
+++ src/main/java/com/example/security/model/Channel.java
@@ -0,0 +1,23 @@
+package com.example.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "channel")
+@Entity
+public class ChannelModel {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
